0847590ed4d0a480e64d70bb1eb2f20b7d979c0f
I9503031124034D9 <I9503031124034D9@GMAIL.COM>
2024-12-15 08:06:01 +0000

    Add files via upload

---
+++ _2/app/index.tsx
@@ -0,0 +1,182 @@
+/* NUMBERING RULES
+IMPORT AS 
+CLOSE TO USE
+CALL DEPTH (EXCHANGE)
+MOVE PARAMETER USE
+*/
+/* TYPING RULES
+IMPORT AS
+CLOSE TO USE
+CALL DEPTH (EXCHANGE)
+NO PARAMETER
+*/
+import
+*
+as
+________47
+from
+'expo-sqlite/kv-store'
+import
+*
+as
+________48
+from
+'expo-router'
+import
+*
+as
+________49
+from
+'expo-status-bar'
+import
+*
+as
+________50
+from
+'react'
+import
+*
+as
+________51
+from
+'react-native'
+import
+*
+as
+________52
+from
+'react-native-svg'
+const
+________53
+=
+________49
+.
+StatusBar
+,
+________54
+=
+________50
+.
+createElement
+,
+________55
+=
+(
+) =>
+________54
+(
+________53
+,
+{
+hidden
+:
+true
+// variables ^ 64
+,
+style
+:
+'auto'
+}
+)
+,
+________56
+=
+________50
+.
+memo
+,
+________57
+=
+________56
+(
+________55
+)
+,
+________58
+=
+________50
+.
+createElement
+,
+________59
+=
+(
+) =>
+________58
+(
+________57
+)
+,
+________60
+=
+________50
+.
+createElement
+,
+________61
+=
+________51
+.
+View
+,
+________62
+=
+(
+) =>
+________60
+(
+________61
+,
+{
+onLayout
+:
+(
+) =>
+{
+// from stor
+}
+}
+,
+________59
+(
+)
+)
+,
+________63
+=
+________50
+.
+memo
+,
+________64
+=
+________63
+(
+________62
+)
+,
+________65
+=
+________50
+.
+createElement
+,
+________66
+=
+(
+) =>
+________65
+(
+________64
+)
+,
+________67
+=
+(
+) =>
+________66
+(
+)
+export
+default
+________67
